d3c661e235c2c3c19876856a938f5a933988e267
Michael Scharff <minka.scharff@gmail.com>
2023-05-09 17:30:35 +0300

    Creating the Resource component

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,10 +1,25 @@
 import React from 'react';
+import Resource from '../resource/Resource';
 
-function App() {
+export default function App() {
+  const date = new Date().toDateString();
+  const resource1 = { name: 'Geomage.com', status: 200, lastActive: 'now', lastTry: date, memoryLeft: '500GB', memoryPrecentage: 80 }
+  const resource2 = { name: '89.192.15.12', status: 200, lastActive: 'now', lastTry: date }
+  const resource3 = { name: 'nebius', status: 404, lastActive: 'Yesterday', lastTry: date, memoryLeft: '300GB', memoryPrecentage: 20 }
+  const resource4 = { name: 'cloud.il', status: 500, lastActive: '13.04.2023 17:26:49', lastTry: date, memoryLeft: '320GB', memoryPrecentage: 40 }
+  const resource5 = { name: '89.192.15.11', status: 208, lastActive: '12.11.2022 03:04:56', lastTry: date }
+
+  const resourceArray = { 'Geomage.com': '1', '89.192.15.12': '2', 'nebius': '3', 'cloud.il': '4', '89.192.15.11': '5' };
+
+  const resourceClick = (name) => {
+    const asd = resourceArray[name];
+    if (asd) {
+      console.log(asd);
+    }
+  }
+
   return (
     <div className="app">
     </div>
   );
 }
-
-export default App;
---
+++ src/components/resource/Resource.js
@@ -0,0 +1,46 @@
+import React from "react";
+import v from '../../images/v.svg';
+import ex from '../../images/ex.svg';
+
+export default function Resource(props) {
+    const { resource, onClick } = props;
+
+    const resourceClick = (evt) => {
+        evt.preventDefault();
+        onClick(resource.name);
+    }
+
+    return (
+        <div className="resource" onClick={resourceClick}>
+            <div className="resource__name_container">
+                <h2 className="resource__header">Name</h2>
+                <h3 className="resource__name">{resource.name}</h3>
+            </div>
+            <div className="resource__status_container">
+                <h2 className="resource__header">Status</h2>
+                <img className="resource__image" src={resource.status === 200 ? v : ex} alt={`Status: ${resource.status}`} title={`Status: ${resource.status}`} />
+                <h3 className={`resource__status ${resource.status === 200 ? '' : 'not-'}working`} >{resource.status}</h3>
+            </div>
+            <div className="resource__last-active_container">
+                <h2 className="resource__header">Last active</h2>
+                <h3 className="resource__last-active">{resource.lastActive}</h3>
+            </div>
+            <div className="resource__last-try_container">
+                <h2 className="resource__header">Last try</h2>
+                <h3 className="resource__last-try">{resource.lastTry}</h3>
+            </div>
+            {
+                resource.memoryLeft ?
+                    <div className="resource__memory_container">
+                        <h2 className="resource__header">Memory</h2>
+                        <div className="resource__memory_content">
+                            <h3 className="resource__memory-left">{resource.memoryLeft}</h3>
+                            <h3 className="resource__memory-precentage">{resource.memoryPrecentage}%</h3>
+                        </div>
+                    </div>
+                    :
+                    <></>
+            }
+        </div>
+    );
+};
