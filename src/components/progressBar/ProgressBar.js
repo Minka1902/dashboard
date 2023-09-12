08768e8f3b44e3759372b968bcd9c358c3f4aaef
Michael scharff <minka.scharff@gmail.com>
2023-09-12 16:38:16 +0300

    Fixing component

--- src/components/progressBar/ProgressBar.js
+++ src/components/progressBar/ProgressBar.js
@@ -9,7 +9,7 @@ export default function ProgressBar({ value, maxValue, isProgressBarOk = true })
             <label className="progress-bar__title">Capacity: </label>
             <div className={`progress-bar ${isProgressBarOk ? 'progress-bar__ok' : ''}`}>
                 <div
-                    className={`progress-bar__fill${percentage > 90 ? ' red-bg' : ''}${percentage > 75 && percentage < 90 ? 'orange-bg' : ''}`}
+                    className={`progress-bar__fill${percentage > 90 ? ' red-bg' : ''}${percentage >= 75 && percentage <= 90 ? ' orange-bg' : ''}`}
                     style={{ width: `${percentage}%` }}>
                 </div>
                 {isProgressBarOk && <div className='progress-bar__label progress-bar__label_ok'>{`${percentage.toFixed(2)}%`}</div>}
