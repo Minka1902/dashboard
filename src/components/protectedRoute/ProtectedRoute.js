5eb0a3a3aa3f4eab854c99d177072c406cfb91cb
Michael scharff <minka.scharff@gmail.com>
2023-09-13 17:18:34 +0300

    Adding the redirectTo property to the component

--- src/components/protectedRoute/ProtectedRoute.js
+++ src/components/protectedRoute/ProtectedRoute.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { Route, Redirect } from "react-router-dom";
 
-export default function ProtectedRoute({ children = null, loggedIn = false, ...props }) {
+export default function ProtectedRoute({ children = null, redirectTo = "/", loggedIn = false, ...props }) {
   return (
     <Route {...props}>
-      {loggedIn ? children : <Redirect to="/" />}
+      {loggedIn ? children : <Redirect to={redirectTo} />}
     </Route>
   );
 };
