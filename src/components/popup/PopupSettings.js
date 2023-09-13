7f07b9d9b1ec4d314f1c9a425ea26b45d5f3a571
Michael scharff <minka.scharff@gmail.com>
2023-09-13 13:17:43 +0300

    Adding the ChartFilter to settings

--- src/components/popup/PopupSettings.js
+++ src/components/popup/PopupSettings.js
@@ -7,6 +7,7 @@ export default function PopupSettings({ isOpen, handleSubmit, onClose }) {
     const [yAxis, setYAxis] = React.useState('zero');
     const [theme, setTheme] = React.useState('light');
     const [watch, setWatch] = React.useState('capacity');
+    const [chartFilter, setChartFilter] = React.useState(6);
 
     const changeYAxis = (evt) => {
         evt.preventDefault();
@@ -22,6 +23,13 @@ export default function PopupSettings({ isOpen, handleSubmit, onClose }) {
         }
     };
 
+    const changeFilter = (evt) => {
+        evt.preventDefault();
+        if (evt.target.selectedOptions[0]) {
+            setChartFilter(evt.target.selectedOptions[0].value * 1);
+        }
+    };
+
     const changeTheme = (evt) => {
         evt.preventDefault();
         if (evt.target.selectedOptions[0]) {
@@ -31,7 +39,7 @@ export default function PopupSettings({ isOpen, handleSubmit, onClose }) {
 
     const saveSettings = (evt) => {
         if (evt.type === 'click' || evt.type === 'submit') {
-            const newSettings = { yAxis, theme, watch };
+            const newSettings = { yAxis, theme, watch, chartFilter };
             handleSubmit(newSettings);
         }
     };
@@ -58,6 +66,18 @@ export default function PopupSettings({ isOpen, handleSubmit, onClose }) {
                         <select className="popup__settings-select" onChange={changeWatch} value={watch}>
                             <option>memory</option>
                             <option>capacity</option>
+                        </select>
+                    </label>
+                    <label className="popup__setting">
+                        chart filter:
+                        <select className="popup__settings-select" onChange={changeFilter} value={chartFilter}>
+                            <option>2</option>
+                            <option>6</option>
+                            <option>8</option>
+                            <option>12</option>
+                            <option>16</option>
+                            <option>20</option>
+                            <option>24</option>
                         </select>
                     </label>
                 </PopupWithForm>
