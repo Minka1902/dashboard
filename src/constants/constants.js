87b7618c3cf885b637607c93c9f489296eab3695
Michael scharff <minka.scharff@gmail.com>
2023-09-14 17:45:04 +0300

    Deleting irrelevant data and Adding the delay constant

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -2,14 +2,7 @@ export const monthArray = ['January', 'February', 'March', 'April', 'May', 'June
 export const shortMonthArray = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
 export const shortMonthArrayLow = ['jan', 'feb', 'mar', 'apr', 'may', 'jun', 'jul', 'aug', 'sep', 'oct', 'nov', 'dec'];
 
-const date = new Date().toDateString();
-const resource1 = { name: 'Geomage.com', status: 200, lastActive: 'now', lastTry: date, memoryLeft: 500, totalMemory: 8000 }
-const resource2 = { name: '89.192.15.12', status: 200, lastActive: 'now', lastTry: date }
-const resource3 = { name: 'nebius', status: 404, lastActive: 'Yesterday', lastTry: date, memoryLeft: 300, totalMemory: 2000 }
-const resource4 = { name: 'cloud.il', status: 500, lastActive: '13.04.2023 17:26:49', lastTry: date, memoryLeft: 320, totalMemory: 4000 }
-const resource5 = { name: '89.192.15.11', status: 208, lastActive: '12.11.2022 03:04:56', lastTry: date }
-
-export const resources = [resource1, resource2, resource3, resource4, resource5];
+export const delay = (seconds) => new Promise((res) => setTimeout(res, seconds * 1000));
 
 export const memoryUnits = ['BYTES', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
 
